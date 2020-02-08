95143963c16a81b16ec595ae2ce152f946442fe7
Rishabh Kumar <diam@adobe.com>
2020-02-08 16:03:13 +0530

    added sample example for nlp code

--- src/nltk/test/test_nltk.py
+++ src/nltk/test/test_nltk.py
@@ -1,3 +1,9 @@
+import urllib
+
+import nltk
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
+
 from nltk.corpus import brown
 import os
 print(brown.words())
@@ -5,3 +11,24 @@ print(len(brown.words()))
 
 if "NLTK_DATA" in os.environ:
     print(os.environ.get("NLTK_DATA"))
+
+
+def get_data(url):
+    response = urllib.request.urlopen(url)
+    html = response.read()
+    soup = BeautifulSoup(html, "html.parser")
+
+    text = soup.get_text(strip=True)
+    tokens = [t for t in text.split()]
+    print(tokens)
+    freq = nltk.FreqDist(tokens)
+    freq.plot(20, cumulative=False, title='Work Count Token')
+
+    stop_words = stopwords.words('english')
+    clean_tokens = [t for t in tokens if t not in stop_words]
+    freq = nltk.FreqDist(clean_tokens)
+    freq.plot(20, cumulative=False, title='Work Count Clean Token')
+
+
+if __name__ == '__main__':
+    get_data('http://php.net/')
